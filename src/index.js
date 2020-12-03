8fecdfb29714e6b7aa4050e0cffe2a0316258c55
Ruslan Robotko <robotko.ruslan@gmail.com>
2020-12-03 20:58:29 +0200

    03.12.2020

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import App from "./components/app/";
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 25,
+      nationality: "Ukraine"
+    };
+    this.nextYear = this.nextYear.bind(this)
+  }
+  nextYear() {
+    this.setState(state => ({
+      years: ++state.years
+    }))
+  }
+  render() {
+    const { name, surname, link } = this.props;
+    const { years, nationality} = this.state;
+    return (
+      <>
+        <button onClick={this.nextYear}>++</button>
+        <h1>
+          My name is {name}, surname - {surname}, years - {years}, nationality - {nationality}
+        </h1>
+        <a href={link}>My profile</a>
+      </>
+    );
+  }
+}
+
+const All = () => {
+  return (
+    <>
+      <WhoAmI name="Ruslan" surname="Robotko" link="google.com" />
+      <WhoAmI name="Oleg" surname="BNuhjk" link="google.com" />
+      <WhoAmI name="Lena" surname="Dkjl" link="google.com" />
+    </>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <All />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
